61cab9376ec4d864a27f912558657213eae3cee2
SureshYarlanki <143301284+SureshYarlanki@users.noreply.github.com>
2025-02-28 12:18:25 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
+const App = () => {
+  const [beers, setBeers] = useState([]);
+  const [search, setSearch] = useState("");
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("https://api.sampleapis.com/beers/ale");
+      setBeers(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      return [];
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const filteredBeers = beers.filter((beer) =>
+    beer.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h1 className="font-bold text-black text-3xl p-4 bg-gray-200 mx-auto text-center">
+        Catalog
+      </h1>
+      <input
+        type="text"
+        className="outline-none border rounded-lg px-4 py-2 w-64 transition-all duration-300 hover:w-80 hover:shadow-md mt-4 ml-5"
+        placeholder="Search here"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <button className="text-white bg-blue-600 ml-4   rounded-md px-4 py-2 ">
+        Search
+      </button>
+      <div className="grid gap-7 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-4 w-[85%] mx-auto">
+        {filteredBeers.map((item) => (
+          <div key={item.id} className="border  rounded-md items-center">
+            <div className=" bg-gray-100 mb-4">
+              <img
+                src={item.image}
+                alt={item.name || "Default Beer Image"}
+                className="h-44 max-w-full m-auto object-contain"
+              />
+            </div>
+            <div className="px-4 py-1">
+              <h2 className="font-bold"> {item.name}</h2>
+              <ul className="mt-2 ">
+                <li>
+                  <span className="font-black">Price:</span> {item.price}
+                </li>
+                <li>
+                  {" "}
+                  <span className="font-black">Average:</span>{" "}
+                  {item.rating.average}
+                </li>
+                <li>
+                  <span className="font-black">reviews:</span>{" "}
+                  {item.rating.reviews}
+                </li>
+              </ul>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
