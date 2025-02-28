312b0a9ca386955a8cbf4b16c220160fe415d606
SureshYarlanki <143301284+SureshYarlanki@users.noreply.github.com>
2025-02-28 12:25:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import React, { useState, useEffect } from "react";
 const App = () => {
   const [beers, setBeers] = useState([]);
   const [search, setSearch] = useState("");
-
+  const [loading, setLoading]=useState(false)
   const fetchData = async () => {
+    setLoading(true);
     try {
       const response = await axios.get("https://api.sampleapis.com/beers/ale");
       setBeers(response.data);
     } catch (error) {
       console.error("Error fetching data:", error);
-      return [];
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -24,7 +26,9 @@ const App = () => {
   );
 
   return (
-    <div>
+  
+    < >
+      
       <h1 className="font-bold text-black text-3xl p-4 bg-gray-200 mx-auto text-center">
         Catalog
       </h1>
@@ -38,37 +42,43 @@ const App = () => {
       <button className="text-white bg-blue-600 ml-4   rounded-md px-4 py-2 ">
         Search
       </button>
-      <div className="grid gap-7 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-4 w-[85%] mx-auto">
-        {filteredBeers.map((item) => (
-          <div key={item.id} className="border  rounded-md items-center">
-            <div className=" bg-gray-100 mb-4">
-              <img
-                src={item.image}
-                alt={item.name || "Default Beer Image"}
-                className="h-44 max-w-full m-auto object-contain"
-              />
+      {
+        (
+          loading ? (
+            <p className="text-center text-xl font-bold mt-6">Loading...</p>
+          ) :
+          <div className="grid gap-7 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-4 w-[85%] mx-auto">
+          {filteredBeers.map((item) => (
+            <div key={item.id} className="border  rounded-md items-center">
+              <div className=" bg-gray-100 mb-4">
+                <img
+                  src={item.image}
+                  alt={item.name || "Default Beer Image"}
+                  className="h-44 max-w-full m-auto object-contain"
+                />
+              </div>
+              <div className="px-4 py-1">
+                <h2 className="font-bold"> {item.name}</h2>
+                <ul className="mt-2 ">
+                  <li>
+                    <span className="font-black">Price:</span> {item.price}
+                  </li>
+                  <li>
+                    {" "}
+                    <span className="font-black">Average:</span>{" "}
+                    {item.rating.average}
+                  </li>
+                  <li>
+                    <span className="font-black">reviews:</span>{" "}
+                    {item.rating.reviews}
+                  </li>
+                </ul>
+              </div>
             </div>
-            <div className="px-4 py-1">
-              <h2 className="font-bold"> {item.name}</h2>
-              <ul className="mt-2 ">
-                <li>
-                  <span className="font-black">Price:</span> {item.price}
-                </li>
-                <li>
-                  {" "}
-                  <span className="font-black">Average:</span>{" "}
-                  {item.rating.average}
-                </li>
-                <li>
-                  <span className="font-black">reviews:</span>{" "}
-                  {item.rating.reviews}
-                </li>
-              </ul>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
+          ))}
+        </div>)
+     }
+    </>
   );
 };
 
